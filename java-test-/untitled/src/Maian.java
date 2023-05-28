fe160fe5ad75ef7112979a57979ef814383f6cc4
buddhathemanoj <134469946+buddhathemanoj@users.noreply.github.com>
2023-05-28 10:09:13 -0700

    Add files via upload

---
+++ java-test-/untitled/src/Maian.java
@@ -0,0 +1,94 @@
+import java.awt.*;
+import java.util.Scanner;
+
+/*  public class Maian {
+    public static void main(String[] ars){
+        int Bike =20000;
+        int Car = 200000;
+        if(Car>Bike){
+            System.out.println("Car will cost more than bike");
+        }
+    }
+}
+
+
+
+public class Maian {
+    public static void main(String[] ars) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number  ");
+        double a = scanner.nextInt();
+
+        System.out.println(a+ " your number");
+
+        scanner.close();
+    }
+}
+
+                                                        */
+/*
+public class Maian {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter the 1st number");
+        int A= scanner.nextInt();
+        System.out.println("enter the second number");
+        int B= scanner.nextInt();
+        System.out.println( "the maximum number between the given numbers are"+" "+ (Math.max( A,  B)));
+        System.out.println("The minimum number between the given numbers are" + "  " + (Math.min(A,B)));
+        scanner.close();
+
+
+    }
+
+}
+                                                                   */
+ /*
+public class Maian {
+    public static void main(String[] args){
+        for (int i=0; i<=6;++i){
+            System.out.println("Outer num" + i);
+        }
+        for(int j=0; j<=3; ++j){
+            System.out.println("Inner num" + j);
+        }
+    }
+}
+                                                                     */
+/*
+public class Maian {
+public static void main(String[] args) {
+   double a = 23.3233;
+  int b = (int)a;
+  byte C = (byte) a;
+    System.out.println(b);
+    System.out.println(C);
+
+    byte A= 121;
+    int F = 12;
+    float D= A;
+    System.out.println(D);
+    System.out.println("THE MAXIMUM NUMBER IS" + " " + Math.max(A,F));
+
+
+}
+}
+                                                                   */
+
+public class Maian {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int a = scanner.nextInt();
+        System.out.println('a' + " +" + " " + '5' + " " + "is:" + (a += 5));
+        System.out.println("Enter the 2nd number");
+        int b = scanner.nextInt();
+        scanner.close();
+        int i= b + 2;
+        System.out.println("THe number " + "2" +"+"+" " +"is" +" "+ i);
+
+    }
+
+
+}
+
